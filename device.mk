a6f08a0c9e9cf28b7a177d41f7204f4c01842740
Marijn Suijten <marijns95@gmail.com>
2021-02-08 22:27:40 +0100

    device: Delete shared copy rules for sensor configuration
    We have never had copy rules nor sensor files in device-specific trees.
    These files are supplied by `prebuilt_etc` declarations in common (no
    need for a copy rule, just depend on the file name which is a "prebuilt"
    package) and generically included by the platform repository; Edo is no
    exception and has them added in [1]. The (list of) included files is
    identical between PDX203 and PDX206.
    
    [1]: https://github.com/sonyxperiadev/device-sony-edo/pull/19

--- device.mk
+++ device.mk
@@ -23,40 +23,6 @@ PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
 
-# Sensors Configuration
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_ak991x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_ak991x_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_ak991x_0_somc_platform.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_ak991x_0_somc_platform.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_ak991x_0_somc_product.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_ak991x_0_somc_product.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_bmp380_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_bmp380_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_bu52053nvx_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_bu52053nvx_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_default_sensors.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_default_sensors.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_dynamic_sensors.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_dynamic_sensors.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_hdk_ak991x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_hdk_ak991x_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_hdk_lsm6dst_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_hdk_lsm6dst_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_hdk_lsm6dst_1.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_hdk_lsm6dst_1.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_irq.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_irq.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_lps22hh_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_lps22hh_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_lsm6dsm_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_lsm6dsm_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_lsm6dsm_0_somc_product.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_lsm6dsm_0_somc_product.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_lsm6dst_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_lsm6dst_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_lsm6dst_1.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_lsm6dst_1.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_power_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_power_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_qrd_ak991x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_qrd_ak991x_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_qrd_lsm6dst_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_qrd_lsm6dst_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_qrd_sx932x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_qrd_sx932x_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_qrd_tmd2725_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_qrd_tmd2725_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_shtw2_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_shtw2_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_somc_default_sensors.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_somc_default_sensors.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_stk3x3x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_stk3x3x_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_svr_bma4_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_svr_bma4_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_svr_bmg160_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_svr_bmg160_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_svr_icm4x6xx_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_svr_icm4x6xx_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_svr_rpr0521rs_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_svr_rpr0521rs_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_sx932x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_sx932x_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/kona_tmd2725_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_tmd2725_0.json \
-    $(DEVICE_PATH)/vendor/etc/sensors/config/sns_device_orient_somc_platform.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/sns_device_orient_somc_platform.json
-
 # Audio Configuration
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/vendor/etc/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
@@ -101,40 +67,6 @@ PRODUCT_PACKAGES += \
 # SAR
 PRODUCT_PACKAGES += \
     TransPowerSensors
-
-# Generic Sensors Configuration
-PRODUCT_PACKAGES += \
-    sns_distance_bound.json \
-    sns_mag_cal_legacy.json \
-    sns_rotv.json \
-    sns_aont.json \
-    sns_dae.json \
-    sns_amd_sw_enabled.json \
-    sns_pedometer.json \
-    sns_mag_cal.json \
-    sns_basic_gestures.json \
-    sns_cm.json \
-    sns_multishake.json \
-    sns_rmd.json \
-    sns_ccd_v2_walk.json \
-    sns_amd_sw_disabled.json \
-    sns_tilt.json \
-    sns_tilt_sw_disabled.json \
-    sns_ccd.json \
-    sns_bring_to_ear.json \
-    sns_diag_filter.json \
-    sns_amd.json \
-    sns_tilt_to_wake.json \
-    sns_gyro_cal.json \
-    sns_tilt_sw_enabled.json \
-    sns_dpc.json \
-    sns_smd.json \
-    sns_geomag_rv.json \
-    sns_facing.json \
-    lsm6dsm_0.json \
-    stk3x3x_0.json \
-    sns_fmv.json \
-    sns_device_orient.json
 
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
